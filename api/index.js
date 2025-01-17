8f4d83528074858c99939ecd8c8a67adceaedeee
mrnonames123 <121599477+mrnonames123@users.noreply.github.com>
2025-01-17 22:19:57 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,11 +9,9 @@ import contactRoute from "./routes/contact.js"
 import confirmbRoute from "./routes/confirmb.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
-
 const app=express()
 dotenv.config()
  
-
 const connect =async()=>{
     try{
         await mongoose.connect(process.env.MONGO);
@@ -24,22 +22,18 @@ const connect =async()=>{
     
     }
 };
-
 mongoose.connection.on("disconnected",()=>{
     console.log("mongo disconnected")
 })
-
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
-
 app.use("/api/auth",authRoute);
 app.use("/api/halls",hallsRoute);
 app.use("/api/slots",slotsRoute);
 app.use("/api/users",usersRoute);
 app.use("/api/contact",contactRoute)
 app.use("/api/confirmb",confirmbRoute)
-
 app.use((err,req,res,next)=>{
     const errorStatus=err.status||500;
     const errorMessage=err.message||"Something went wrong"
@@ -50,9 +44,7 @@ app.use((err,req,res,next)=>{
         stack:err.stack,
     });
 });
-
 app.listen(8080,listen=>{
     connect()
     console.log('connected to server..')
 })
-
